e42e93e910216f907496457aeb3a3c0bb7c32a17
pisakhov <pisakhov@gmail.com>
2023-04-04 23:36:39 -0400

    remove wrap

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,22 @@
+const markdownIt = require("markdown-it");
+const md = new markdownIt();
+
 module.exports = function(eleventyConfig) {
 
-    eleventyConfig.addPassthroughCopy("./src/style.css");
-    eleventyConfig.addPassthroughCopy("./src/assets");
-    eleventyConfig.addPassthroughCopy("./src/admin");
+  eleventyConfig.addPassthroughCopy("./src/style.css");
+  eleventyConfig.addPassthroughCopy("./src/assets");
+  eleventyConfig.addPassthroughCopy("./src/admin");
 
-    return {
-        dir: {
-            input: "src",
-            output: "public"
-        }
-    };
-}
+  // Custom filter to prevent wrapping content with <p></p> tags
+  eleventyConfig.addFilter("unwrapParagraph", (content) => {
+    let html = md.render(content);
+    return html.replace(/^<p>|<\/p>$/g, "");
+  });
+
+  return {
+    dir: {
+      input: "src",
+      output: "public"
+    }
+  };
+};
