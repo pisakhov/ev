a81dc5e9bf2c8cd8972e61ab792a21dfd97b4835
pisakhov <pisakhov@gmail.com>
2023-04-11 16:54:21 -0400

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,8 @@
-const markdownIt = require("markdown-it");
-
 module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("./src/style.css");
   eleventyConfig.addPassthroughCopy("./src/assets");
   eleventyConfig.addPassthroughCopy("./src/admin");
-  eleventyConfig.addPassthroughCopy("./src/admin/config.yml");
-
 
   return {
     dir: {
