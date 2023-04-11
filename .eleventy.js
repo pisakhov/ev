3a3d781d1804be7fe3fd9c7c3a24a2fe17662d6e
pisakhov <64985264+pisakhov@users.noreply.github.com>
2023-04-11 16:13:53 -0400

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/style.css");
   eleventyConfig.addPassthroughCopy("./src/assets");
   eleventyConfig.addPassthroughCopy("./src/admin");
+  eleventyConfig.addPassthroughCopy("./src/admin/config.yml");
 
 
   return {
