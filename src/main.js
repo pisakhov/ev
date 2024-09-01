749b9c60976dd900ed618ae3eb5ca6b186098030
pisakhov <pisakhov@gmail.com>
2024-08-31 20:02:21 -0400

    e

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
-import { MathfieldElement } from 'mathlive';
+import { MathfieldElement } from "mathlive";
 const mfe = new MathfieldElement();
-// Use mfe as needed in your code
-// const cannot be used since nerdamer gets modified when other modules are loaded
-var nerdamer = require('nerdamer');
-// Load additional modules. These are not required.
-require('nerdamer/Algebra');
-require('nerdamer/Calculus');
-require('nerdamer/Solve');
-require('nerdamer/Extra');
+
+// Use var for nerdamer as mentioned in your comment
+var nerdamer = require("nerdamer");
+require("nerdamer/Algebra");
+require("nerdamer/Calculus");
+require("nerdamer/Solve");
+require("nerdamer/Extra");
+
+// Export the objects you want to use in your HTML
+export { mfe, nerdamer };
